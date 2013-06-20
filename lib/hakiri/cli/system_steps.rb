8456f69cef12b882c58aa3a5f9cbcdc9a1ccde47
Vasily Vasinov <des.elyon@gmail.com>
2013-06-20 14:54:17 -0600

    Show vulnerabilities in system:steps

--- lib/hakiri/cli/system_steps.rb
+++ lib/hakiri/cli/system_steps.rb
@@ -34,7 +34,6 @@ class Hakiri::SystemSteps < Hakiri::Cli
 
     say '-----> Retrieving software versions versions on your system...'
 
-    @stack = Hakiri::@stack.new()
     @stack.build_from_input(server, extra_server, db, redis, memcached)
     @stack.fetch_versions
 
@@ -43,6 +42,38 @@ class Hakiri::SystemSteps < Hakiri::Cli
     else
       @stack.technologies.each do |technology_name, payload|
         say "-----> Found #{technology_name} #{payload[:version]}"
+      end
+
+      say '-----> Searching for vulnerabilities...'
+      params = ({ technologies: @stack.technologies }.to_param)
+      response = @http_client.get_issues(params)
+      authenticated = response[:meta][:authenticated]
+
+      if response[:technologies].empty?
+        say '-----> No vulnerabilities found. Keep it up!'
+      else
+        response[:technologies].each do |technology|
+          unless technology[:issues_count] == 0
+            say "-----> Found #{technology[:issues_count].to_i} #{'vulnerability'.pluralize if technology[:issues_count].to_i != 1} in #{technology[:technology][:name]} #{technology[:version]}"
+            puts ' '
+          end
+        end
+
+        if authenticated
+          if agree 'Show all of them? (yes or no) '
+            puts ' '
+            response[:technologies].each do |technology|
+              technology[:issues].each do |issue|
+                say issue[:name]
+                say issue[:description]
+                puts ' '
+              end
+            end
+          end
+        else
+          say '****** Signup on www.hakiriup.com and make your command line requests with an auth_token, so you can see issues that your technologies have.'
+          say '****** You will also receive notifications via email whenever new issues are found.'
+        end
       end
     end
   end
