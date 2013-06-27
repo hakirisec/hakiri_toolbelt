4e49f89bc6c2025ea1a17333acb5848cef2a4bdf
Vasily Vasinov <des.elyon@gmail.com>
2013-06-27 11:37:49 -0600

    cli text fix

--- lib/hakiri/cli/manifest.rb
+++ lib/hakiri/cli/manifest.rb
@@ -8,7 +8,7 @@ class Hakiri::Manifest < Hakiri::Cli
     FileUtils::copy_file "#{File.dirname(__FILE__)}/manifest.json", "#{Dir.pwd}/manifest.json"
     File.chmod 0755, "#{Dir.pwd}/manifest.json"
     say '-----> Generating the manifest file...'
-    say "-----> Generated the manifest file in #{Dir.pwd}/manifest.json"
-    say "-----> Edit it and run \"hakiri system:scan\""
+    say "       Generated the manifest file in #{Dir.pwd}/manifest.json"
+    say "       Edit it and run \"hakiri system:scan\""
   end
 end
