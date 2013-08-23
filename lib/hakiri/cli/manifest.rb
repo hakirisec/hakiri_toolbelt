29a383008d64238c895441374f167ae85f07fd79
Vasily Vasinov <des.elyon@gmail.com>
2013-08-22 22:08:33 -0600

    grammar

--- lib/hakiri/cli/manifest.rb
+++ lib/hakiri/cli/manifest.rb
@@ -7,8 +7,8 @@ class Hakiri::Manifest < Hakiri::Cli
   def generate
     FileUtils::copy_file "#{File.dirname(__FILE__)}/manifest.json", "#{Dir.pwd}/manifest.json"
     File.chmod 0755, "#{Dir.pwd}/manifest.json"
-    say '-----> Generating the manifest file...'
-    say "       Generated the manifest file in #{Dir.pwd}/manifest.json"
+    say '-----> Generating a manifest file...'
+    say "       Generated a manifest file in #{Dir.pwd}/manifest.json"
     say "       Edit it and run \"hakiri system:scan\""
   end
 end
