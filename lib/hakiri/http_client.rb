58d96adfb27a901977e12159257b85e9510010c0
Vasily Vasinov <des.elyon@gmail.com>
2014-02-03 20:50:54 -0700

    Pass auth token in headers

--- lib/hakiri/http_client.rb
+++ lib/hakiri/http_client.rb
@@ -8,6 +8,7 @@ class Hakiri::HttpClient
   #
   def initialize
     @auth_token = (ENV['HAKIRI_AUTH_TOKEN'] or nil)
+    @headers = { 'X-AUTH-TOKEN' => @auth_token }
     @api_url = (ENV['HAKIRI_API_URL'] or 'https://hakiri.io/api/v1')
   end
 
@@ -21,9 +22,8 @@ class Hakiri::HttpClient
   #   Returns a hash of technologies with vulnerabilities.
   #
   def get_issues(params)
-    params[:auth_token] = @auth_token
-
-    RestClient.post "#{@api_url}/issues/scan.json", params do |response, request, result, &block|
+    RestClient::Request.execute method: :post, url: "#{@api_url}/issues/scan.json",
+                                headers: @headers, payload: params do |response, request, result, &block|
       case response.code
         when 200
           JSON.parse(response.to_str, :symbolize_names => true)
@@ -46,9 +46,8 @@ class Hakiri::HttpClient
   #   Returns a hash of differences between technologies.
   #
   def check_versions_diff(stack_id, params)
-    params[:auth_token] = @auth_token
-
-    RestClient.post "#{@api_url}/stacks/#{stack_id}/versions/diffs.json", params do |response, request, result, &block|
+    RestClient::Request.execute method: :post, url: "#{@api_url}/stacks/#{stack_id}/versions/diffs.json",
+                                headers: @headers, payload: params do |response, request, result, &block|
       case response.code
         when 200
           JSON.parse(response.to_str, :symbolize_names => true)
@@ -71,9 +70,8 @@ class Hakiri::HttpClient
   #   Returns a hash of updated versions.
   #
   def sync_stack_versions(stack_id, params)
-    params[:auth_token] = @auth_token
-
-    RestClient.put "#{@api_url}/stacks/#{stack_id}/versions/update_all.json", params do |response, request, result, &block|
+    RestClient::Request.execute method: :put, url: "#{@api_url}/stacks/#{stack_id}/versions/update_all.json",
+                                headers: @headers, payload: params do |response, request, result, &block|
       case response.code
         when 200
           JSON.parse(response.to_str, :symbolize_names => true)
@@ -93,7 +91,8 @@ class Hakiri::HttpClient
   #   Returns a hash with build fields, repository fields and an array of warnings.
   #
   def code_report(stack_id)
-    RestClient.get "#{@api_url}/stacks/#{stack_id}/builds/last.json?auth_token=#{@auth_token}" do |response, request, result, &block|
+    RestClient::Request.execute method: :get, url: "#{@api_url}/stacks/#{stack_id}/builds/last.json",
+                                headers: @headers, payload: params do |response, request, result, &block|
       case response.code
         when 200
           JSON.parse(response.to_str, :symbolize_names => true)
